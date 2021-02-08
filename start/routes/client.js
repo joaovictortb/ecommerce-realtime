fed1e12374360eec359abdd9a9c8ccb179cb3fcd
joaovictortb <joaovictorofc@gmail.com>
2021-02-08 13:08:35 -0300

    created order and product controllers

--- start/routes/client.js
+++ start/routes/client.js
@@ -4,9 +4,13 @@
 const Route = use('Route');
 
 Route.group(() => {
-  Route.resource('products', 'ProductController').apiOnly();
+  Route.get('products', 'ProductController.index');
+  Route.get('products/:id', 'ProductController.show');
 
-  Route.resource('orders', 'OrderController').apiOnly();
+  Route.get('orders', 'OrderController.index');
+  Route.get('orders/:id', 'OrderController.show');
+  Route.post('orders', 'OrderController.store');
+  Route.put('orders/:id', 'OrderController.put');
 })
   .prefix('v1')
   .namespace('Client');
